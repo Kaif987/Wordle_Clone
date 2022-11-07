a313ab17b8676325e2eb027bc72331c86625d43d
Kaif Siddiqui <mdkaifprofession@gmail.com>
2022-11-07 10:21:38 +0530

    setup port 3001 for db.json using express

---
+++ src/Backend/express.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+const data = require("./db.json")
+
+const server = app.get("/solution", (req, res) =>{
+    res.header({"Content-Type" : "Application/json"})
+    res.send(JSON.stringify(data))
+})
+
+server.listen(3001, () =>{
+    console.log("Server now running on port 3001")
+})
